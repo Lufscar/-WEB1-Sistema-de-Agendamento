e69c065f35253b22d64ce24527ac196a1b4aed1c
Lufscar <luciana.losg@gmail.com>
2021-09-16 13:02:59 -0300

    Atualiza BD

--- SistemaAgendamento/db/MySQL/create.sql
+++ SistemaAgendamento/db/MySQL/create.sql
@@ -3,34 +3,37 @@ create database AgendamentoConsultas;
 use AgendamentoConsultas;
 
 create table CLIENTES(
-	cpf decimal(11) constraint CLIENTES_pk primary key,
+	cpf decimal(11) not null,
 	email varchar(128) not null,
 	senha varchar(128) not null, 
 	nome varchar(256) not null,
 	telefone varchar(13),
-	sexo char(2), --F (feminino), M (masculino), NB (não binário) ou ND (não declarado)
-	nascimento date --Formato padrão: “aaaa-mm-dd”
+	sexo char(2),
+	nascimento date,
+	primary key (cpf)
 	);
 
 create table PROFISSIONAIS(
-	cpf decimal(11) constraint PROFISSIONAIS_pk primary key,
+	cpf decimal(11) not null,
 	email varchar(128) not null,
 	senha varchar(64) not null, 
 	nome varchar(128) not null,
-	area varchar(128) not null, --(medicina, advocacia, psicologia) 
-	especialidade varchar(128) not null --(médico cardiologista, advogado criminal, psicólogo infantil, etc)
-	);
+	area varchar(128) not null,
+	especialidade varchar(128) not null,
+	primary key (cpf)
+    );
 
 create table CONSULTAS(
-	id bigint not null auto_increment constraint CONSULTAS_pk primary key,
+	id bigint not null auto_increment,
 
-	cpf_C decimal(11),  
+	cpf_C decimal(11) not null,  
 	constraint CLIENTES_fk foreign  key (cpf_C) references CLIENTES(cpf),
 
-	cpf_P decimal(11),  
+	cpf_P decimal(11) not null,  
 	constraint PROFISSIONAIS_fk foreign  key (cpf_P) references PROFISSIONAIS(cpf),
 
-	agendamento datetime --Formato padrão: “aaaa-mm-dd hh:mm:ss" --FALTA COLOCAR RESTRIÇÕES
+	agendamento datetime, 
+    primary key (cpf)
 	);
 
 insert into CLIENTES(cpf, email, senha, nome) 
@@ -52,10 +55,10 @@ insert into PROFISSIONAIS(cpf, email, senha, nome, area, especialidade)
 	values  ('33344455566', 'agiacomini@estudante.ufscar.br', '1234', 'Anderson Henrique', 'psicologia', 'psicólogo infantil');
 
 insert into CONSULTAS(cpf_C, cpf_P, agendamento)
-	values ('11122233344', '22233344455', '2021-10-10 10:00:00')
+	values ('11122233344', '22233344455', '2021-10-10 10:00:00');
 
 insert into CONSULTAS(cpf_C, cpf_P, agendamento)
-	values ('22233344455', '33344455566', '2021-10-11 11:00:00')
+	values ('22233344455', '33344455566', '2021-10-11 11:00:00');
 
 insert into CONSULTAS(cpf_C, cpf_P, agendamento)
-	values ('22233344455', '11122233344', '2021-10-12 12:00:00')
+	values ('22233344455', '11122233344', '2021-10-12 12:00:00');
